2a2d863515b95b380afa5670d95b5f2576ae74be
Juan Sánchez <JuanchoSL@hotmail.com>
2025-06-08 17:31:06 +0200

    methods documentation

--- src/DataConverters/JsonConverter.php
+++ src/DataConverters/JsonConverter.php
@@ -1,17 +1,23 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace JuanchoSL\DataTransfer\DataConverters;
 
 class JsonConverter extends AbstractConverter
 {
 
+    /**
+     * Retrieve the data as an json string
+     * @return bool|string
+     */
     public function getData(): mixed
     {
         return json_encode($this->data);
     }
-    
+
+    /**
+     * Return the json representation of the entity
+     * @return bool|string
+     */
     public function __tostring(): string
     {
         return $this->getData();
--- src/DataConverters/YamlConverter.php
+++ src/DataConverters/YamlConverter.php
@@ -1,6 +1,4 @@
-<?php
-
-declare(strict_types=1);
+<?php declare(strict_types=1);
 
 namespace JuanchoSL\DataTransfer\DataConverters;
 
@@ -9,6 +7,11 @@ use JuanchoSL\Exceptions\PreconditionRequiredException;
 class YamlConverter extends ArrayConverter
 {
 
+    /**
+     * Parse and returns the the yaml string composition
+     * @throws \JuanchoSL\Exceptions\PreconditionRequiredException
+     * @return string
+     */
     public function getData(): mixed
     {
         if (!function_exists('yaml_emit')) {
@@ -17,6 +20,11 @@ class YamlConverter extends ArrayConverter
         return trim(yaml_emit(parent::getData(), YAML_UTF8_ENCODING), "-\r\n.");
     }
 
+    /**
+     * Returns the the yaml string composition
+     * @throws \JuanchoSL\Exceptions\PreconditionRequiredException
+     * @return string
+     */
     public function __tostring(): string
     {
         return $this->getData();
